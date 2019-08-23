2ee28ddb30acf38cac6b0ccec968eca92bcc4407
Pat Palmerston <patpalmerston@gmail.com>
2019-08-23 12:03:27 -0700

    colors displayed

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -10,12 +10,12 @@ const BubblePage = () => {
 	// fetch your colors data from the server when the component mounts
 	// set that data to the colorList state property
 
-	const getColors = () => {
-    axiosWithAuth()
-      .get('http://localhost:5000/api/colors')
+	useEffect(() => {
+		axiosWithAuth()
+			.get('http://localhost:5000/api/colors')
 			.then(res => setColorList(res.data))
 			.catch(err => console.error(err));
-	};
+	}, []);
 
 	return (
 		<>
